6bf0dc579f3aa10e452428d03d6fc8df7a4295e4
SuperKXT <superkxt@outlook.com>
2024-04-20 14:51:33 +0500

    Add `apisix-token.lua`

---
+++ apisix-token.lua
@@ -0,0 +1,17 @@
+local token = os.getenv("APISIX_TOKEN")
+
+if not token then
+  ngx.log(ngx.ERR, "APISIX_TOKEN environment variable not found")
+  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
+local access_phase = ngx.router.access
+
+access_phase = function(ctx)
+  ngx.req.set_header("apisix-token", token)
+
+  -- Call the original access phase function
+  access_phase(ctx)
+end
+
+ngx.router.access = access_phase
